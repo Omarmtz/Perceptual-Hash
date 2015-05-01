f902615381443dce6de412b755530e7e9cace846
= <omack47@gmail.com>
2015-05-01 14:41:55 -0500

    Fixed Main Code

--- LocalSearchEngine/Program.cs
+++ LocalSearchEngine/Program.cs
@@ -13,15 +13,7 @@ namespace LocalSearchEngine
     {
         static void Main()
         {
-            var patterns = new string[] {"*.*"};
-            var files = GetFiles(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), patterns);
-            foreach (string file in files)
-            {
-                Console.WriteLine(file);
-            }
-            Console.WriteLine("Files found {0}",files.Count);
-
-            Console.ReadLine();
+            
         }
 
         
