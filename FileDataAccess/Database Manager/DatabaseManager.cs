5a450a3b6f52e20c5f8959967a24bbf78cb27bdc
= <omack47@gmail.com>
2015-07-10 00:38:59 -0500

    Copyright Comment Author

--- FileDataAccess/Database Manager/DatabaseManager.cs
+++ FileDataAccess/Database Manager/DatabaseManager.cs
@@ -1,4 +1,17 @@
-﻿using System;
+﻿// <copyright>
+// Copyright (c) 2015, All Right Reserved
+//
+// THIS CODE AND INFORMATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY 
+// KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A
+// PARTICULAR PURPOSE.
+//
+// </copyright>
+// <author>Omar Martínez Rosas</author>
+// <email>omack47@gmail.com</email>
+// <date>3-07-2015</date>
+
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
